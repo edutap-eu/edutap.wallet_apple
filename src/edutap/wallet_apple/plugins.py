f3bf22ab28663eeaf97faf42a0fa9899eeadad2c
Robert Niederreiter <rnix@squarewave.at>
2024-12-12 14:23:44 +0100

    Validate plugins in lookup functions

--- src/edutap/wallet_apple/plugins.py
+++ src/edutap/wallet_apple/plugins.py
@@ -6,9 +6,17 @@ from typing import Iterable
 
 def get_pass_registrations() -> Iterable[PassRegistration]:
     eps = entry_points(group="edutap.wallet_apple.plugins")
-    return [ep.load() for ep in eps if ep.name == "PassRegistration"]
+    plugins = [ep.load() for ep in eps if ep.name == "PassRegistration"]
+    for plugin in plugins:
+        if not isinstance(plugin, PassRegistration):
+            raise ValueError(f"{plugin} not implements PassRegistration")
+    return [plugin() for plugin in plugins]
 
 
 def get_pass_data_acquisitions() -> Iterable[PassDataAcquisition]:
     eps = entry_points(group="edutap.wallet_apple.plugins")
-    return [ep.load() for ep in eps if ep.name == "PassDataAcquisition"]
+    plugins = [ep.load() for ep in eps if ep.name == "PassDataAcquisition"]
+    for plugin in plugins:
+        if not isinstance(plugin, PassDataAcquisition):
+            raise ValueError(f"{plugin} not implements PassDataAcquisition")
+    return [plugin() for plugin in plugins]
