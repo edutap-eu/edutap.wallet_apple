5117dd71dbb0e99c500af67a001e204f554f931a
Philipp Auersperg-Castell <phil@bluedynamics.com>
2024-12-21 14:03:17 +0100

    env

--- src/edutap/wallet_apple/settings.py
+++ src/edutap/wallet_apple/settings.py
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_prefix="EDUTAP_WALLET_APPLE_",
         case_sensitive=False,
-        # env_file=".env",
+        env_file=".env",
         # env_file_encoding="utf-8",
         extra="allow",
     )
