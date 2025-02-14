64349a9819e6f8c7ee7019440641cae8b187b7fe
Philipp Auersperg-Castell <phil@bluedynamics.com>
2025-02-14 12:52:24 +0100

    runtime-plugin-registration

--- src/edutap/wallet_apple/plugins.py
+++ src/edutap/wallet_apple/plugins.py
@@ -4,10 +4,31 @@ from .protocols import PassRegistration
 from importlib.metadata import entry_points
 
 
+_PLUGIN_CLASS_NAMES = {
+    "PassDataAcquisition": PassDataAcquisition,
+    "PassRegistration": PassRegistration,
+    "Logging": Logging,
+}
+_PLUGIN_REGISTRY: dict[str, list[PassDataAcquisition | PassRegistration | Logging]] = {}
+
+
+def add_plugin(
+    name: str, plugin: PassDataAcquisition | PassRegistration | Logging
+) -> None:
+
+    if not isinstance(plugin, _PLUGIN_CLASS_NAMES[name]):
+        raise TypeError(f"{plugin} not implements {name}")
+    if name not in _PLUGIN_REGISTRY:
+        _PLUGIN_REGISTRY.setdefault(name, [])
+        _PLUGIN_REGISTRY[name].append(plugin)
+
+
 def get_pass_registrations() -> list[PassRegistration]:
     eps = entry_points(group="edutap.wallet_apple.plugins")
     # allow multiple entries by searching for the prefix
-    plugins = [ep.load() for ep in eps if ep.name.startswith("PassRegistration")]
+    plugins = [
+        ep.load() for ep in eps if ep.name.startswith("PassRegistration")
+    ] + _PLUGIN_REGISTRY.get("PassRegistration", [])
     if not plugins:
         raise NotImplementedError("No pass registration plug-in found")
     for plugin in plugins:
@@ -19,7 +40,9 @@ def get_pass_registrations() -> list[PassRegistration]:
 def get_pass_data_acquisitions() -> list[PassDataAcquisition]:
     eps = entry_points(group="edutap.wallet_apple.plugins")
     # here we only allow one entry, so we search for the exact name
-    plugins = [ep.load() for ep in eps if ep.name == "PassDataAcquisition"]
+    plugins = [
+        ep.load() for ep in eps if ep.name == "PassDataAcquisition"
+    ] + _PLUGIN_REGISTRY.get("PassDataAcquisition", [])
     if not plugins:
         raise NotImplementedError("No pass data acquisition plug-in found")
     for plugin in plugins:
@@ -31,7 +54,9 @@ def get_pass_data_acquisitions() -> list[PassDataAcquisition]:
 def get_logging_handlers() -> list[Logging]:
     eps = entry_points(group="edutap.wallet_apple.plugins")
     # allow multiple entries by searching for the prefix
-    plugins = [ep.load() for ep in eps if ep.name.startswith("Logging")]
+    plugins = [
+        ep.load() for ep in eps if ep.name.startswith("Logging")
+    ] + _PLUGIN_REGISTRY.get("Logging", [])
     # if not plugins:
     #     raise NotImplementedError("No logging plug-in found")
     for plugin in plugins:
--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -1,3 +1,54 @@
+from edutap.wallet_apple.models import handlers
+from edutap.wallet_apple.plugins import get_logging_handlers
+from edutap.wallet_apple.plugins import get_pass_data_acquisitions
+from edutap.wallet_apple.plugins import get_pass_registrations
+
+import pytest
+
+
+class DummyPassRegistration:
+    async def pass_registration(self):
+        pass
+
+    async def register_pass(
+        self,
+        device_libray_id: str,
+        pass_type_id: str,
+        serial_number: str,
+        push_token: handlers.PushToken | None,
+    ) -> None: ...
+
+    async def unregister_pass(
+        self, device_library_id: str, pass_type_id: str, serial_number: str
+    ) -> None: ...
+
+
+class DummyPassDataAcquisition:
+    async def get_pass_data(
+        self, *, pass_type_id: str | None, serial_number: str, update: bool = False
+    ) -> handlers.PassData:
+        raise NotImplementedError
+
+    async def get_push_tokens(
+        self, device_library_id: str | None, pass_type_id: str, serial_number: str
+    ) -> list[handlers.PushToken]:
+        raise NotImplementedError
+
+    async def get_update_serial_numbers(
+        self, device_library_id: str, pass_type_id: str, last_updated: str | None = None
+    ) -> handlers.SerialNumbers:
+        raise NotImplementedError
+
+    async def check_authentication_token(
+        self, pass_type_id: str | None, serial_number: str | None, token: str
+    ) -> bool:
+        raise NotImplementedError
+
+
+class DummyLogging:
+    async def log(self, entries: handlers.LogEntries) -> None: ...
+
+
 def test_get_pass_registrations(entrypoints_testing):
     from edutap.wallet_apple.plugins import get_pass_registrations
     from edutap.wallet_apple.protocols import PassRegistration
@@ -23,3 +74,19 @@ def test_get_logging_handlers(entrypoints_testing):
     plugins = get_logging_handlers()
     assert len(plugins) == 2
     assert isinstance(plugins[0], Logging)
+
+
+def test_add_plugin():
+    from edutap.wallet_apple.plugins import add_plugin
+
+    add_plugin("PassRegistration", DummyPassRegistration)
+
+    # check for TypeError for wrong plugin type
+    with pytest.raises(TypeError):
+        add_plugin("PassDataAcquisition", DummyPassRegistration)
+    add_plugin("PassDataAcquisition", DummyPassDataAcquisition)
+    add_plugin("Logging", DummyLogging)
+
+    assert len(get_pass_registrations()) == 1
+    assert len(get_pass_data_acquisitions()) == 1
+    assert len(get_logging_handlers()) == 1
